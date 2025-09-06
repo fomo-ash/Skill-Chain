06d4271c8ac8aff05930799edf53c74e5a5aa726
Nipuna <nipunamahakur@gmail.com>
2025-09-07 00:28:32 +0530

    Error

--- skill-passport/frontend/src/pages/Home/LoginPage/LoginPage.jsx
+++ skill-passport/frontend/src/pages/Home/LoginPage/LoginPage.jsx
@@ -64,53 +64,6 @@ const LoginPage = ({ handleLoginSuccess }) => {
           <button>Apple</button>
         </div>
       </div>
-
-        <form onSubmit={handleLogin}>
-          <div className="inputs">
-            <input
-              className="input"
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-            <input
-              className="input"
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-
-          {error && <p style={{ color: "red", textAlign: 'center', marginTop: '1rem' }}>{error}</p>}
-
-          <div className="signup-btn">
-            <button type="submit" disabled={loading}>
-              {loading ? "Logging in..." : "Login"}
-            </button>
-
-            <p className="para">Or continue with</p>
-            <div className="acc-btns">
-              <button type="button">
-                <img src="/facebook.png" alt="Facebook" />
-              </button>
-              <button type="button">
-                <img src="/devicon_google.png" alt="Google" />
-              </button>
-              <button type="button">
-                <img src="/apple.png" alt="Apple" />
-              </button>
-            </div>
-             <p className="para" style={{ marginTop: '1rem' }}>
-                Don't have an account? <Link to="/signup">Sign Up</Link>
-            </p>
-          </div>
-        </form>
       </div>
 
       <div className="left-side">
